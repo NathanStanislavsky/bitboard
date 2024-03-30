de21c4849febfc540a5f41daa96557d56366a564
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2024-03-30 17:59:27 -0400

    masked sliding pieces moves

--- move_gen.cpp
+++ move_gen.cpp
@@ -8,14 +8,60 @@
 // A column
 const BB not_A_column = 18374403900871474942ULL;
 
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+
+// 1111111011111110111111101111111011111110111111101111111011111110
+
+
 // H column
 const BB not_H_column = 9187201950435737471ULL;
+
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+// 11111110
+
+// 111111101111111011111110111111101111111011111110111111101111111
 
 // HG column
 const BB not_HG_column = 4557430888798830399ULL;
 
+// 11111100
+// 11111100
+// 11111100
+// 11111100
+// 11111100
+// 11111100
+// 11111100
+// 11111100
+
+// 11111100111111001111110011111100111111001111110011111100111111
+
 // AB column
 const BB not_AB_column = 18229723555195321596ULL;
+
+// 0 0 1 1 1 1 1 1
+// 0 0 1 1 1 1 1 1
+// 0 0 1 1 1 1 1 1
+// 0 0 1 1 1 1 1 1
+// 0 0 1 1 1 1 1 1 
+// 0 0 1 1 1 1 1 1
+// 0 0 1 1 1 1 1 1
+// 0 0 1 1 1 1 1 1
+
+// 1111110011111100111111001111110011111100111111001111110011111100
+
 
 // Generate pawn attacks
 BB mask_pawn_attacks(Color side, Square square) {
@@ -23,7 +69,7 @@ BB mask_pawn_attacks(Color side, Square square) {
     BB attacks = 0ULL;
 
     // piece bitboard
-    BB bitboard = 0ULL;
+    BB bitboard = 0ULL; 
 
     // set piece on board
     set_bit(bitboard, square);
@@ -31,20 +77,24 @@ BB mask_pawn_attacks(Color side, Square square) {
     // white pawns
     if (!side) {
         if ((bitboard << 7) & not_H_column) {
+            // attack south west
             attacks |= (bitboard << 7);
         }
 
         if ((bitboard << 9) & not_A_column) {
+            // attack south east
             attacks |= (bitboard << 9);
         }
     }
     // black pawns
     else {
         if ((bitboard >> 7) & not_A_column) {
-            attacks |= (bitboard >> 7);
+            // attack north east
+            attacks |= (bitboard >> 7);            
         }
 
         if ((bitboard >> 9) & not_H_column) {
+            // attack north west
             attacks |= (bitboard >> 9);
         }
     }
@@ -65,6 +115,7 @@ BB mask_knight_attacks(Square square) {
     set_bit(bitboard, square);
 
     // Generate knight attacks
+    //up
     if ((bitboard >> 17) & not_H_column) {
         attacks |= (bitboard >> 17);
     }
@@ -77,6 +128,8 @@ BB mask_knight_attacks(Square square) {
     if ((bitboard >> 6) & not_AB_column) {
         attacks |= (bitboard >> 6);
     }
+
+    // down
     if ((bitboard << 17) & not_A_column) {
         attacks |= (bitboard << 17);
     }
@@ -105,33 +158,145 @@ BB mask_king_attacks(Square square) {
     // set piece on board
     set_bit(bitboard, square);
     
+    // north
     if (bitboard >> 8) {
         attacks |= (bitboard >> 8);
     }
+    // north west
     if ((bitboard >> 9) & not_H_column) {
         attacks |= (bitboard >> 9);
     }
+    // north east
     if ((bitboard >> 7) & not_A_column) {
         attacks |= (bitboard >> 7);
     }
+    // west
     if ((bitboard >> 1) & not_H_column) {
         attacks |= (bitboard >> 1);
     }
-
+    
+    // south
     if (bitboard << 8) {
         attacks |= (bitboard << 8);
     }
+    // south east 
     if ((bitboard << 9) & not_A_column) {
         attacks |= (bitboard << 9);
     }
+    // south west
     if ((bitboard << 7) & not_H_column) {
         attacks |= (bitboard << 7);
     }
+    // east
     if ((bitboard << 1) & not_A_column) {
         attacks |= (bitboard << 1);
     }
     
     // return attack map
+    return attacks;
+}
+
+BB mask_bishop_attacks(int square, BB block) {
+    // result attacks bitboard
+    BB attacks = 0ULL;
+    
+    // init row & file
+    int r, f;
+    
+    // init target rank & files
+    int tr = square / 8;
+    int tf = square % 8;
+    
+    // south east
+    for (r = tr + 1, f = tf + 1; r <= 7 && f <= 7; r++, f++)
+    {
+        attacks |= (1ULL << (r * 8 + f));
+        if ((1ULL << (r * 8 + f)) & block) 
+            break;
+    }
+    
+    // north east
+    for (r = tr - 1, f = tf + 1; r >= 0 && f <= 7; r--, f++)
+    {
+        attacks |= (1ULL << (r * 8 + f));
+        if ((1ULL << (r * 8 + f)) & block) 
+            break;
+    }
+    
+    // south west
+    for (r = tr + 1, f = tf - 1; r <= 7 && f >= 0; r++, f--)
+    {
+        attacks |= (1ULL << (r * 8 + f));
+        if ((1ULL << (r * 8 + f)) & block)
+            break;
+    }
+    
+    // north west
+    for (r = tr - 1, f = tf - 1; r >= 0 && f >= 0; r--, f--)
+    {
+        attacks |= (1ULL << (r * 8 + f));
+        if ((1ULL << (r * 8 + f)) & block)
+            break;
+    }
+    
+    // return attack map
+    return attacks;
+}
+
+BB mask_rook_attacks(int square, BB block)
+{
+    // result attacks bitboard
+    BB attacks = 0ULL;
+    
+    // init ranks & files
+    int r, f;
+    
+    // init target rank & files
+    int tr = square / 8;
+    int tf = square % 8;
+    
+    // down
+    for (r = tr + 1; r <= 7; r++)
+    {
+        attacks |= (1ULL << (r * 8 + tf));
+        if ((1ULL << (r * 8 + tf)) & block) 
+            break;
+    }
+    // up
+    for (r = tr - 1; r >= 0; r--)
+    {
+        attacks |= (1ULL << (r * 8 + tf));
+        if ((1ULL << (r * 8 + tf)) & block) 
+            break;
+    }
+    // right
+    for (f = tf + 1; f <= 7; f++)
+    {
+        attacks |= (1ULL << (tr * 8 + f));
+        if ((1ULL << (tr * 8 + f)) & block) 
+            break;
+    }
+    // left
+    for (f = tf - 1; f >= 0; f--)
+    {
+        attacks |= (1ULL << (tr * 8 + f));
+        if ((1ULL << (tr * 8 + f)) & block) 
+            break;
+    }
+    
+    // return attack map
+    return attacks;
+}
+
+// generate rook attacks on the fly
+BB mask_queen_attacks(int square, BB block)
+{
+    // result attacks bitboard
+    BB attacks = 0ULL;
+    
+    attacks |= mask_rook_attacks(square, block);
+    attacks |= mask_bishop_attacks(square, block);
+
     return attacks;
 }
 
