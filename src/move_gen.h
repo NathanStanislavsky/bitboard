bd1abc74ec3712c6cf99a58ee0487e459dfbc497
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2024-12-19 23:18:37 -0500

    added functions to header file

--- src/move_gen.h
+++ src/move_gen.h
@@ -15,5 +15,13 @@ BB mask_knight_attacks(Square square);
 // Generate pawn attacks
 BB mask_king_attacks(Square square);
 
+BB mask_king_attacks(Square square);
+
+BB mask_bishop_attacks(int square, BB block);
+
+BB mask_rook_attacks(int square, BB block);
+
+BB mask_queen_attacks(int square, BB block);
+
 // generate leaper piece attacks
 void init_leapers_attacks();
