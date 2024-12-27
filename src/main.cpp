d3ed8c2892776fd0a1a2b90b082f8351187996db
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2024-12-27 15:06:18 -0500

    test refactor

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,6 @@
 
 int main()
 {
-    // Pos pos("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
-    // pos.print_board();
-
-    // cout << perft(pos, 4, true) << endl;
-
     test_fens();
     
     return 0;
