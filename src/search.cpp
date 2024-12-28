e980f929ffc03dab7fb986d53fce9f0d7e958674
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2024-12-28 17:02:02 -0500

    initial search function

--- src/search.cpp
+++ src/search.cpp
@@ -4,24 +4,25 @@
 
 #include <map>
 
-
 std::map<Piece, int> point_value = {
     {PAWN, 100},
     {BISHOP, 300},
     {KNIGHT, 300},
     {ROOK, 500},
-    {QUEEN, 900}
-};
+    {QUEEN, 900}};
 
-int count_material(Pos& pos, Color side) {
+int count_material(Pos &pos, Color side)
+{
     int material = 0;
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 6; i++)
+    {
         material += __builtin_popcount(pos.pieces_bbs[i] & pos.colors_bbs[side]) * point_value[Piece(i)];
     }
     return material;
 }
 
-int eval(Pos& pos) {
+int eval(Pos &pos)
+{
     Color side = pos.turn;
     Color enemy = Color(!side);
 
@@ -29,4 +30,43 @@ int eval(Pos& pos) {
     int enemy_player_material = count_material(pos, enemy);
 
     return current_player_material - enemy_player_material;
+}
+
+int search(Pos &pos, int depth)
+{
+    if (depth == 0)
+    {
+        return eval(pos);
+    }
+
+    vector<Move> legal_moves = generate_legal_moves(pos);
+
+    // checkmate/stalemate
+    if (legal_moves.size() == 0)
+    {
+        if (pos.is_in_check(pos.turn))
+        {
+            return -10000;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+
+    // maximize the score
+    int max = -10000;
+    for (Move move : legal_moves)
+    {
+        pos.doMove(move);
+        int eval = -search(pos, depth - 1);
+        pos.undoMove();
+
+        if (eval > max)
+        {
+            max = eval;
+        }
+    }
+    return max;
 }
--- src/search.h
+++ src/search.h
@@ -1,3 +1,7 @@
 #pragma once
 
 int eval(Pos& pos);
+
+int count_material(Pos& pos, Color side);
+
+int search(Pos& pos, int depth);
