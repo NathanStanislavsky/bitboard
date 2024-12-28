9c6d971e18794ca0151e1490ab32e6536a58bc5c
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2024-12-28 15:01:53 -0500

    removed incorrect tests and fixed ones with incorrect results

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@ int main()
 {
     init_leapers_attacks();
 
-    // Pos pos("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
+    // Pos pos("");
     // pos.print_board();
 
     // Timestamp start = get_current_ms();
 
-    // cout << perft(pos, 5, true) << endl;
+    // cout << perft(pos, 6, true) << endl;
 
     // print_time_diff(start);
 
